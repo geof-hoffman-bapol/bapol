7e351dc934a87244c9ff5dd6af5c3f81b05dc6f5
JavaG23 <geof.hoffman@gmail.com>
2024-08-04 00:05:49 -0400

    updated _id type to string

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -1,14 +1,17 @@
 import { Schema, model, models } from "mongoose";
 
+// Define the User schema with _id as a string
 const UserSchema = new Schema({
+  _id: { type: String, required: true }, // Define _id as a string
   clerkId: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   username: { type: String, required: true, unique: true },
   firstName: { type: String, required: true },
-  lastName: {type: String, required: true },
+  lastName: { type: String, required: true },
   photo: { type: String, required: true },
-})
+});
 
+// Create or use the existing User model
 const User = models.User || model('User', UserSchema);
-            //mongoose.model('User', UserSchema);
+
 export default User;
